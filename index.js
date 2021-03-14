055eb97a04afc566721ca707988dd399c906486e
Francesco Brandozzi <francesco.brandozzi@gmail.com>
2021-03-14 10:13:07 +0000

    x

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { chromium } = require("playwright")
 
 ;(async () => {
+  console.log(1)
   const browser = await chromium.launch()
+  console.log(2)
   const page = await browser.newPage()
+  console.log(3)
   await page.goto("https://google.com")
+  console.log(4)
   await browser.close()
+  console.log(5)
 })()
